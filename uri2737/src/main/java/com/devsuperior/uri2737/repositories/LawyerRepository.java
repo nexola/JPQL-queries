769d3ee469bebc52226a0d23fbdc0fb9b2e3e9ee
nexola <vitormatheusfv@gmail.com>
2024-01-14 12:47:20 -0300

    URI 2990 LEFT JOIN - DIFERENÇA

--- uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,10 +1,8 @@
 package com.devsuperior.uri2737.repositories;
 
-import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.entities.Lawyer;
 import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devsuperior.uri2737.entities.Lawyer;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
